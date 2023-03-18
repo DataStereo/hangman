2fb2f8bd67383c4c27226fc38911c391dabb4669
Oleh Morozov <morozov89oleg@gmail.com>
2023-03-18 22:50:31 +0200

    added a new field to the random constructor

--- src/RandomWord.java
+++ src/RandomWord.java
@@ -1,10 +1,13 @@
+import java.util.Random;
+
 public class RandomWord {
     private String[] words = {"father", "family", "infant", "tongue", "explicit", "authority", "tombstone", "blacksmith", "likeness", "photographs", "regarding", "unreasonably", "character", "inscription", "conclusion", "brothers", "exceedingly", "religiously", "ed", "existence"};
 
     private String chosenWord;
+    private Random random = new Random();
 
     public RandomWord(){
-        chosenWord = words[1];
+        chosenWord = words[random.nextInt(words.length)];
     }
     public String toString(){
         return chosenWord;
